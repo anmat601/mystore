399c9458e3730b93277b5883681d89cec669273d
anumat601 <anumathew.601@gmail.com>
2018-02-23 15:00:14 -0600

    Configured CXF with Spring

--- MyStoreWeb/src/MyStoreWeb/GetCitiesByCountryServlet.java
+++ MyStoreWeb/src/MyStoreWeb/GetCitiesByCountryServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mystore.bean.BeanManager;
 import net.webservicex.GlobalWeather;
 import net.webservicex.GlobalWeatherSoap;
 
@@ -31,8 +32,12 @@ public class GetCitiesByCountryServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		GlobalWeather globalWeather =new GlobalWeather();
+		/*GlobalWeather globalWeather =new GlobalWeather();
 		GlobalWeatherSoap gs= globalWeather.getGlobalWeatherSoap();
+		String country= gs.getCitiesByCountry("India");
+		response.getWriter().append(country);*/
+		
+		GlobalWeatherSoap gs =(GlobalWeatherSoap)BeanManager.getContext().getBean("client");
 		String country= gs.getCitiesByCountry("India");
 		response.getWriter().append(country);
 	}
